7d753719304862f3a1e2f63eb500a43ee89fd2af
venilson1 <venilson2009@hotmail.com>
2022-05-25 18:38:15 -0300

    migrations

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: "127.0.0.1",
+      port: 3306,
+      user: "root",
+      password: "root",
+      database: "system_vote",
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+  },
+};
---
+++ src/database/migrations/20220525210953_create_table_survey.js
@@ -0,0 +1,12 @@
+exports.up = (knex) =>
+  knex.schema.createTable("tb_survey", (table) => {
+    table.increments("id");
+    table.text("title").notNullable();
+    table.datetime("inital_date", { precision: 6 }).defaultTo(knex.fn.now(6));
+    table.datetime("final_date", { precision: 6 }).defaultTo(knex.fn.now(6));
+
+    table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("update_at").notNullable().defaultTo(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable("tb_survey");
