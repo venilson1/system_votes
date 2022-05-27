7da0d2fe6579bb844af18a557da67e9154cf3750
venilson1 <venilson2009@hotmail.com>
2022-05-27 03:33:27 -0300

    block dates finish and not started

--- frontend/src/pages/voting/Votes.jsx
+++ frontend/src/pages/voting/Votes.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { helpersDefinitionStatus } from '../../helpers/helpersDefinitionStatus';
 import { helpersFormatDate } from '../../helpers/helpersFormatDate';
 import api from '../../services/api';
 
 export default function Votes() {
 
   const [surveys, setSurveys] = useState();
-
 
   useEffect(() => {
     api
@@ -31,42 +31,80 @@ export default function Votes() {
         {
           surveys?.map(el => {
             return (
-              <div key={el.id} className="max-w-sm rounded overflow-hidden shadow-xl w-1/3">
-                <div className="px-6 py-4 rounded-t-lg bg-zinc-900 text-center">
-                  <span className="font-bold text-sm  mb-2  text-zinc-100">{el.title}</span>
-                </div>
-                <div className="px-6 pt-4 pb-2 flex flex-col">
-                  {
-                    el.answers.map(el => {
+              <>
+                {
+                  helpersDefinitionStatus(el.initial_date, el.final_date) === 'andamento' ? (
+                    <div key={el.id} className="bg-violet-400 max-w-sm rounded overflow-hidden shadow-xl w-1/3">
+                      <div className="px-6 py-4 rounded-t-lg bg-zinc-900 text-center">
+                        <span className="font-bold text-sm  mb-2  text-zinc-100">{el.title}</span>
+                      </div>
+                      <div className="px-6 pt-4 pb-2 flex flex-col">
+                        {
+                          el.answers.map(el => {
 
-                      JSON.parse(el)
+                            JSON.parse(el)
 
-                      return (
-                        <div onClick={() => handlerUpdateVotes(JSON.parse(el).id)} key={JSON.parse(el).id} className={`bg-violet-200 hover:bg-violet-400 rounded-full px-3 py-1 mb-2 flex justify-between cursor-pointer`}>
-                          <span className="text-sm font-semibold text-zinc-700 ml-2">{JSON.parse(el).field}</span>
-                          <span className="text-sm font-semibold text-zinc-700 flex items-end">votos: {JSON.parse(el).votes}</span>
-                        </div>
-                      )
-                    })
-                  }
-                </div>
-                <div className='flex justify-items-end justify-around text-xs font-bold py-4'>
-                  <span className="flex flex-col justify-center text-center space-y-1">
-                    <p>início</p>
-                    <p className="bg-fuchsia-200 px-8 py-1 rounded-2xl text-slate-900">{helpersFormatDate(el.initial_date)}</p>
-                  </span>
-                  <span className="flex flex-col justify-center text-center space-y-1">
-                    <p>encerramento</p>
-                    <p className="bg-fuchsia-200 px-8 py-1 rounded-2xl text-slate-900">{helpersFormatDate(el.final_date)}</p>
-                  </span>
-                </div>
-              </div>
+                            return (
+                              <div onClick={() => handlerUpdateVotes(JSON.parse(el).id)} key={JSON.parse(el).id} className={`bg-violet-200 hover:bg-violet-500 rounded-full px-3 py-1 mb-2 flex justify-between cursor-pointer`}>
+                                <span className="text-sm font-semibold text-zinc-700 ml-2">{JSON.parse(el).field}</span>
+                                <span className="text-sm font-semibold text-zinc-700 flex items-end">votos: {JSON.parse(el).votes}</span>
+                              </div>
+                            )
+                          })
+                        }
+                      </div>
+                      <div className='flex justify-items-end justify-around text-xs font-bold py-4'>
+                        <span className="flex flex-col justify-center text-center space-y-1">
+                          <p>início</p>
+                          <p className="bg-fuchsia-200 px-8 py-1 rounded-2xl text-slate-900">{helpersFormatDate(el.initial_date)}</p>
+                        </span>
+                        <span className="flex flex-col justify-center text-center space-y-1">
+                          <p>encerramento</p>
+                          <p className="bg-fuchsia-200 px-8 py-1 rounded-2xl text-slate-900">{helpersFormatDate(el.final_date)}</p>
+                        </span>
+                      </div>
+                    </div>
+                  ) : (
+                    <div key={el.id} className="max-w-sm rounded overflow-hidden shadow-xl w-1/3 cursor-not-allowed">
+                      <div className="px-6 py-4 rounded-t-lg bg-zinc-900 text-center">
+                        <span className="font-bold text-sm  mb-2  text-zinc-100">{el.title}</span>
+                      </div>
+                      <div className="px-6 pt-4 pb-2 flex flex-col">
+                        {
+                          el.answers.map(el => {
+
+                            JSON.parse(el)
+
+                            return (
+                              <div key={JSON.parse(el).id} className={`bg-gray-200 rounded-full px-3 py-1 mb-2 flex justify-between`}>
+                                <span className="text-sm font-semibold text-zinc-400 ml-2">{JSON.parse(el).field}</span>
+                                <span className="text-sm font-semibold text-zinc-400 flex items-end">votos: {JSON.parse(el).votes}</span>
+                              </div>
+                            )
+                          })
+                        }
+                      </div>
+                      <div className='flex justify-items-end justify-around text-xs font-bold py-4'>
+                        <span className="flex flex-col justify-center text-center space-y-1">
+                          <p className="text-gray-200">início</p>
+                          <p className="bg-gray-200 px-8 py-1 rounded-2xl text-gray-400">{helpersFormatDate(el.initial_date)}</p>
+                        </span>
+                        <span className="flex flex-col justify-center text-center space-y-1">
+                          <p className="text-gray-200">encerramento</p>
+                          <p className="bg-gray-200 px-8 py-1 rounded-2xl text-gray-400">{helpersFormatDate(el.final_date)}</p>
+                        </span>
+                      </div>
+                      <span className="flex justify-center font-bold text-red-500">{helpersDefinitionStatus(el.initial_date, el.final_date)}</span>
+                    </div>
+                  )
+                }
+
+              </>
 
             )
           })
         }
       </div>
-
     </>
   )
 }
